33bc8387e0f8acdfba8fb47094aaf287d34c55c7
HJDEV <hj.hjdev@gmail.com>
2021-01-16 01:12:49 +0900

    Fix marker size is too big

--- front/src/store/map.js
+++ front/src/store/map.js
@@ -88,8 +88,8 @@ const mutations = {
   },
   ADD_MARKER(state, data) {
     // default marker
-    let imageSize = new kakao.maps.Size(58, 84), // 마커이미지의 크기입니다
-        imageOption = {offset: new kakao.maps.Point(29, 84)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.     
+    let imageSize = new kakao.maps.Size(29, 42), // 마커이미지의 크기입니다
+        imageOption = {offset: new kakao.maps.Point(15, 42)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.     
     // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
     let markerImage = new kakao.maps.MarkerImage(state.nullImage, imageSize, imageOption);
     // create marker
@@ -125,7 +125,7 @@ const mutations = {
 
     state.tempImage = new kakao.maps.MarkerImage(
       state.image,
-      new kakao.maps.Size(58, 84), new kakao.maps.Point(29, 84)
+      new kakao.maps.Size(29, 42), new kakao.maps.Point(15, 42)
     );
     
     state.markerIndex = state.markers.findIndex(x => x.deviceId === data.deviceId);
